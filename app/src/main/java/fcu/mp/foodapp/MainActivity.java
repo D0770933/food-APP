3cc35c6226e3f3f3895054f9eff8f23d6469c471
D0770933 <coach51315@gmail.com>
2022-06-10 12:21:08 +0800

    final

---
+++ app/src/main/java/fcu/mp/foodapp/MainActivity.java
@@ -0,0 +1,33 @@
+package fcu.mp.foodapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+
+        Intent intent = new Intent();
+        intent.setClass(this,Login.class);
+        startActivity(intent);
+
+
+    }
+
+
+}
